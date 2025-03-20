775703149a2f409b42625e4b2b10d1c673f44c5b
Austin Fite <akfite@gmail.com>
2025-03-19 23:49:15 -0400

    Expanding overload functionality

--- src/graphics/ReferenceFrame3d.m
+++ src/graphics/ReferenceFrame3d.m
@@ -11,10 +11,9 @@ classdef ReferenceFrame3d < handle
         h_frame
     end
     
-    %% General
+    %% Constructors
     methods
         function this = ReferenceFrame3d(dcm, origin)
-
             arguments
                 dcm(3,3) double {mustBeReal} = eye(3)
                 origin(1,3) double {mustBeReal} = [0 0 0]
@@ -24,28 +23,71 @@ classdef ReferenceFrame3d < handle
             this.origin = origin;
         end
 
-        function update(this, dcm, origin)
-            %UPDATE Update the reference frame's parameters.
+        function set(this, dcm, origin)
+            %SET Set new reference frame parameters.
 
-            arguments
-                this(1,1) ReferenceFrame3d
-                dcm(3,3) double {mustBeReal}
-                origin(1,3) double {mustBeReal} = this.origin
+            switch class(dcm)
+                case 'ReferenceFrame3d'
+                    frame = dcm;
+                    this.dcm = frame.dcm;
+                    this.origin = frame.origin;
+                case {'double','single'}
+                    this.dcm = dcm;
+                    this.origin = origin;
+                otherwise
+                    validateattributes(dcm,{'single','double','ReferenceFrame3d'},{});
             end
 
-            this.dcm = dcm;
-            this.origin = origin;
-
             this.update_transform();
+        end
+
+        function tform_4x4 = homogeneous_transform(this)
+            %HOMOGENEOUS_TRANSFORM Convert to 4x4 form (rotation & translation)
+            tform_4x4 = this.dcm; % 3x3
+            tform_4x4(4,4) = 1; % grow to 4x4
+            tform_4x4 = tform_4x4 * makehgtform('translate', this.origin);
+        end
+
+        function C = mtimes(A, B)
+            %MTIMES Overload for matrix multiplication.
+            if isa(B,'ReferenceFrame3d')
+                B_matrix = B.homogeneous_transform();
+            elseif isnumeric(B)
+                % convert numeric to 4x4 (assume no translation when 3x3)
+                if size(B,1) == 3 && size(B,2) == 3
+                    B_matrix(1:3,1:3) = B;
+                elseif size(B,1) == 4 && size(B,2) == 4
+                    B_matrix = B;
+                else
+                    error('Expected B to be 3x3 or 4x4 when numeric.');
+                end
+            end
+
+            C_matrix = A.homogeneous_transform() * B_matrix;
+            C = ReferenceFrame3d(C_matrix(1:3,1:3), C_matrix(1:3,4));
+        end
+
+        function new = ctranspose(this)
+            new = ReferenceFrame3d(this.dcm.', this.origin);
+        end
+
+        function new = transpose(this)
+            new = ReferenceFrame3d(this.dcm', this.origin);
         end
     end
 
     %% Plotting
     methods
-        function plot(this, ax)
+        function plot(this)
             %PLOT Plot as a 3-d object.
 
+            % DEBUG/TODO: remove
+            figure
             ax = gca;
+            xlim([-1 1]);
+            ylim([-1 1]);
+            zlim([-1 1]);
+            grid on; box on;
 
             if isempty(this.h_transform) || ~isvalid(this.h_transform)
                 % draw from scratch
@@ -54,8 +96,7 @@ classdef ReferenceFrame3d < handle
 
             this.update_transform();
 
-            % TODO: figure out how to plot at a reasonable size
-            scale = 0.1;
+            scale = 0.1 * max(diff(xlim(ax)), diff(ylim(ax)));
 
             % we may wish to plot other things to the reference frame transform,
             % so let's organize the basis vector data under its own group
@@ -70,17 +111,17 @@ classdef ReferenceFrame3d < handle
             line(this.h_frame, ...
                 [0 scale], [0 0], [0 0], ...
                 'Color', 'r', ...
-                'LineWidth', 2, ...
+                'LineWidth', 1, ...
                 'Clipping', 'off');
             line(this.h_frame, ....
                 [0 0], [0 scale], [0 0], ...
                 'Color', 'g', ...
-                'LineWidth', 2, ...
+                'LineWidth', 1, ...
                 'Clipping', 'off');
             line(this.h_frame, ...
                 [0 0], [0 0], [0 scale], ...
                 'Color', 'b', ...
-                'LineWidth', 2, ...
+                'LineWidth', 1, ...
                 'Clipping', 'off');
         end
 
@@ -90,11 +131,7 @@ classdef ReferenceFrame3d < handle
                 return
             end
 
-            tform = this.dcm;
-            tform(4,4) = 1; % make 4x4
-            tform = tform * makehgtform('translate', this.origin);
-
-            this.h_transform.Matrix = tform;
+            this.h_transform.Matrix = homogeneous_transform(this);
         end
     end
 
