3be2196da31d5278666118ff38d80b0b6ef9cff8
Austin Fite <akfite@gmail.com>
2025-03-22 09:46:02 -0400

    Cleanup & reorganize

--- src/ReferenceFrame3d.m
+++ src/ReferenceFrame3d.m
@@ -1,4 +1,4 @@
-classdef ReferenceFrame3d < matlab.mixin.Copyable
+classdef ReferenceFrame3d < matlab.mixin.Copyable & matlab.mixin.CustomDisplay
     
     properties (SetAccess = private)
         T(4,4) double {mustBeReal} = eye(4) % homogeneous transform (rotation & translation)
@@ -18,95 +18,43 @@ classdef ReferenceFrame3d < matlab.mixin.Copyable
         h_frame
     end
 
-    %% matlab.mixin.Copyable
-    methods
-        function new = copyElement(this)
-            new = copyElement@matlab.mixin.Copyable(this);
+    properties (Constant, Hidden)
+        VALIDATION_TOLERANCE = 1e-10
+    end
 
-            % only copy graphics objects if something has been plotted
-            if isempty(this.h_transform) || ~isvalid(this.h_transform)
+    %% Constructors
+    methods
+        function this = ReferenceFrame3d(matrix, origin)
+            if nargin == 0
                 return
             end
-            parent = this.h_transform.Parent;
-            new.h_transform = copyobj(this.h_transform, parent);
-            new.h_frame = copyobj(this.h_frame, parent);
+            if nargin < 2
+                origin = [0 0 0];
+            end
+
+            if isequal(size(matrix,[1 2]), [4 4])
+                this.T = matrix;
+            else
+                matrix(4,4) = 1;
+                this.T = matrix * makehgtform('translate', origin);
+            end
+
+            validate(this);
         end
     end
 
-    %% Dependent
-    methods
-        function v = get.R(this)
-            v = this.T(1:3,1:3);
-        end
-        function v = get.x(this)
-            v = this.T(1:3,1);
-        end
-        function v = get.y(this)
-            v = this.T(1:3,2);
-        end
-        function v = get.z(this)
-            v = this.T(1:3,3);
-        end
-        function v = get.t(this)
-            v = this.T(1:3,4);
-        end
-    end
-
-    methods
-        function [p, dist] = intersect_plane(this, observer, ray, opts)
-            arguments
-                this(1,1) ReferenceFrame3d
-                observer(1,3) double
-                ray(1,3) double
-                opts.Slice(1,1) string = "xy"
-                opts.Offset(1,1) double = 0
-                opts.Debug(1,1) logical = true
-            end
-
-            % select the 2 basis vectors that define the plane
-            switch opts.Slice
-                case "xy"
-                    a = this.x; b = this.y;
-                case "xz"
-                    a = this.x; b = this.z;
-                case "yz"
-                    a = this.y; b = this.z;
-                case "yx"
-                    a = this.y; b = this.x;
-                case "zx"
-                    a = this.z; b = this.x;
-                case "zy"
-                    a = this.z; b = this.y;
-                otherwise
-                    % validatestring
-            end
-
-            normal = cross(a, b);
-            normal = normal ./ norm(normal);
-            point = this.t + (opts.Offset * normal);
-
-            denominator = dot(normal, ray);
-
-            if abs(denominator) < 1e-6
-                p = [nan nan nan];
-                return
-            end
-
-            numerator = dot(normal, point - observer);
-            dist = numerator / denominator;
-            p = observer + (dist * ray);
-
-            if opts.Debug
-                % TODO
-            end
-        end
-    end
-    
-    %% TODO
-    methods
+    %% Math & Utility
+    methods        
         function validate(this)
-            assert(det(this.R) == 1, ...
-                'Expected determinant of R to be 1; instead got %f', det(this.R));
+            R = this.R; %#ok<*PROP>
+            tol = this.VALIDATION_TOLERANCE;
+            assert(abs(det(R) - 1) < tol, ...
+                'Expected determinant of 3x3 submatrix R to be 1; instead got %f', det(R));
+            RRt = R*R';
+            orthogonality_error = norm(RRt - eye(3),'fro');
+            assert(orthogonality_error < tol, ...
+                'Orthogonality is outside tolerance (R*R'' is not eye(3); %f > %f)', ...
+                orthogonality_error, tol)
         end
 
         function new = translate(this, dx)
@@ -156,7 +104,68 @@ classdef ReferenceFrame3d < matlab.mixin.Copyable
             inverse = ReferenceFrame3d([R_inv, t_inv; 0 0 0 1]);
         end
 
-        % overloads
+        function T = as_matrix(this)
+            arguments
+                this(1,1) ReferenceFrame3d
+            end
+            T = this.T;
+        end
+        function as_quaternion()
+        end
+        function [roll, pitch, yaw] = as_euler()
+        end
+
+        function [p, dist] = intersect_plane(this, observer, ray, opts)
+            arguments
+                this(1,1) ReferenceFrame3d
+                observer(1,3) double
+                ray(1,3) double
+                opts.Slice(1,1) string = "xy"
+                opts.Offset(1,1) double = 0
+                opts.Debug(1,1) logical = true
+            end
+
+            % select the 2 basis vectors that define the plane
+            switch opts.Slice
+                case "xy"
+                    a = this.x; b = this.y;
+                case "xz"
+                    a = this.x; b = this.z;
+                case "yz"
+                    a = this.y; b = this.z;
+                case "yx"
+                    a = this.y; b = this.x;
+                case "zx"
+                    a = this.z; b = this.x;
+                case "zy"
+                    a = this.z; b = this.y;
+                otherwise
+                    % validatestring
+            end
+
+            normal = cross(a, b);
+            normal = normal ./ norm(normal);
+            point = this.t + (opts.Offset * normal);
+
+            denominator = dot(normal, ray);
+
+            if abs(denominator) < 1e-6
+                p = [nan nan nan];
+                return
+            end
+
+            numerator = dot(normal, point - observer);
+            dist = numerator / denominator;
+            p = observer + (dist * ray);
+
+            if opts.Debug
+                % TODO
+            end
+        end
+    end
+
+    %% Overloads
+    methods
         function new = mtimes(this, other)
             %MTIMES Matrix multiplication.
             new = ReferenceFrame3d(this.T * other.T);
@@ -170,72 +179,15 @@ classdef ReferenceFrame3d < matlab.mixin.Copyable
             T_new(1:3,1:3) = T_new(1:3,1:3).';
             new = ReferenceFrame3d(T_new);
         end
-
-        % numeric representations
-        function T = as_matrix(this)
-            arguments
-                this(1,1) ReferenceFrame3d
-            end
-            T = this.T;
-        end
-        function as_quaternion()
-        end
-        function [roll, pitch, yaw] = as_euler()
-        end
-    end
-    
-    %% Constructors
-    methods
-        function this = ReferenceFrame3d(matrix, origin)
-            if nargin == 0
-                return
-            end
-            if nargin < 2
-                origin = [0 0 0];
-            end
-
-            if isequal(size(matrix,[1 2]), [4 4])
-                this.T = matrix;
-            else
-                matrix(4,4) = 1;
-                this.T = matrix * makehgtform('translate', origin);
-            end
-        end
-
-        function set(this, dcm, origin)
-            %SET Set new reference frame parameters.
-
-            switch class(dcm)
-                case 'ReferenceFrame3d'
-                    frame = dcm;
-                    this.dcm = frame.dcm;
-                    this.t = frame.t;
-                case {'double','single'}
-                    this.dcm = dcm;
-                    this.t = origin;
-                otherwise
-                    validateattributes(dcm,{'single','double','ReferenceFrame3d'},{});
-            end
-
-            this.update_transform();
-        end
     end
 
-    %% Plotting
+    %% Graphics
     methods
-        function tform = hgtransform(this, parent)
-            if nargin < 2
-                parent = gca;
-            end
-
-            if isempty(this.h_transform) || ~isvalid(this.h_transform)
-                this.h_transform = hgtransform('Parent', parent);
-            end
-
-            tform = this.h_transform;
+        function show(this)
+            % draw everything in a dedicated figure
         end
 
-        function plot(this)
+        function plot(this, opts)
             %PLOT Plot as a 3-d object.
 
             % DEBUG/TODO: remove
@@ -246,8 +198,7 @@ classdef ReferenceFrame3d < matlab.mixin.Copyable
             zlim([-1 1]);
             grid on; box on;
 
-            this.hgtransform(); % TODO: configurable parent
-            this.update_transform();
+            this.get_hgtransform(); % TODO: configurable parent
 
             scale = 0.1 * max(diff(xlim(ax)), diff(ylim(ax)));
 
@@ -278,13 +229,65 @@ classdef ReferenceFrame3d < matlab.mixin.Copyable
                 'Clipping', 'off');
         end
 
-        function update_transform(this)
+        function tform = get_hgtransform(this, parent)
+            %GET_HGTRANSFORM Get (and create, if necessary) a graphics transform.
+            if nargin < 2
+                parent = gca;
+            end
+
+            % TODO: if parent doesn't match current, make a new transform
+            if isempty(this.h_transform) || ~isvalid(this.h_transform)
+                this.h_transform = hgtransform('Parent', parent);
+            end
+
+            this.update_hgtransform();
+            tform = this.h_transform;
+        end
+
+        function update_hgtransform(this)
             %UPDATE_TRANSFORM Re-orient based on the object state.
             if isempty(this.h_transform) || ~isvalid(this.h_transform)
                 return
             end
-
             this.h_transform.Matrix = this.T;
+        end
+    end
+
+    %% Dependent
+    methods
+        function v = get.R(this)
+            v = this.T(1:3,1:3);
+        end
+
+        function v = get.x(this)
+            v = this.T(1:3,1);
+        end
+
+        function v = get.y(this)
+            v = this.T(1:3,2);
+        end
+
+        function v = get.z(this)
+            v = this.T(1:3,3);
+        end
+
+        function v = get.t(this)
+            v = this.T(1:3,4);
+        end
+    end
+
+    %% matlab.mixin.Copyable
+    methods (Access = protected)
+        function new = copyElement(this)
+            new = copyElement@matlab.mixin.Copyable(this);
+
+            % only copy graphics objects if something has been plotted
+            if isempty(this.h_transform) || ~isvalid(this.h_transform)
+                return
+            end
+            parent = this.h_transform.Parent;
+            new.h_transform = copyobj(this.h_transform, parent);
+            new.h_frame = copyobj(this.h_frame, parent);
         end
     end
 
