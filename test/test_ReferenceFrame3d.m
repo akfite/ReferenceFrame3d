5c751ea1b6606f102559a9ebae1989067d1cc91c
Austin Fite <akfite@gmail.com>
2025-04-04 22:12:36 -0400

    Unit test case for local2base

--- test/test_ReferenceFrame3d.m
+++ test/test_ReferenceFrame3d.m
@@ -8,6 +8,16 @@ classdef test_ReferenceFrame3d < matlab.unittest.TestCase
 
         function static_constructors(this)
         end
+
+        function local2base(this)
+            a = ReferenceFrame3d();
+            this.verifyEqual(a.T, eye(4));
+
+            % test some 
+            a.rotate_euler([45 0 0]);
+            vec_out = a.local2base([1/sqrt(2) 1/sqrt(2) 0]);
+            this.verifyEqual(vec_out, [0 1 0], 'AbsTol', eps);
+        end
     end
 
 end
